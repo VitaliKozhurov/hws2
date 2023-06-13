d60214b5201179b3887be6bdea4727dce7874201
VitaliKozhurov <vitalik24021992@gmail.com>
2023-06-13 10:38:59 +0300

    Homework 12

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -2,45 +2,46 @@ import React, {
     SelectHTMLAttributes,
     DetailedHTMLProps,
     ChangeEvent,
-} from 'react'
-import s from './SuperSelect.module.css'
+} from "react";
+import s from "./SuperSelect.module.css";
 
-type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>,
-    HTMLSelectElement>
+type DefaultSelectPropsType = DetailedHTMLProps<
+    SelectHTMLAttributes<HTMLSelectElement>,
+    HTMLSelectElement
+>;
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
-}
+    options?: any[];
+    onChangeOption?: (option: any) => void;
+};
 
 const SuperSelect: React.FC<SuperSelectPropsType> = ({
-                                                         options,
-                                                         className,
-                                                         onChange,
-                                                         onChangeOption,
-                                                         ...restProps
-                                                     }) => {
-
+    options,
+    className,
+    onChange,
+    onChangeOption,
+    ...restProps
+}) => {
     const mappedOptions: any[] = options
         ? options.map((o) => (
-            <option
-                id={'hw7-option-' + o.id}
-                className={s.option}
-                key={o.id}
-                value={o.id}
-            >
-                {o.value}
-            </option>
-        ))
-        : [] // map options with key
+              <option
+                  id={"hw7-option-" + o.id}
+                  className={s.option}
+                  key={o.id}
+                  value={o.id}
+              >
+                  {o.value}
+              </option>
+          ))
+        : []; // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        onChange?.(e) // If we have props onChange
+        onChange?.(e); // If we have props onChange
 
-        onChangeOption?.(e.currentTarget.value)
-    }
+        onChangeOption?.(e.currentTarget.value);
+    };
 
-    const finalSelectClassName = s.select + (className ? ' ' + className : '')
+    const finalSelectClassName = s.select + (className ? " " + className : "");
 
     return (
         <select
@@ -50,7 +51,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
         >
             {mappedOptions}
         </select>
-    )
-}
+    );
+};
 
-export default SuperSelect
+export default SuperSelect;
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,25 @@
 const initState = {
     themeId: 1,
-}
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (
+    state: { themeId: number } = initState,
+    action: ChangeThemeActionType
+): { themeId: number } => {
     switch (action.type) {
-        // дописать
+        case "SET_THEME_ID":
+            return { themeId: Number(action.id) };
 
         default:
-            return state
+            return state;
     }
-}
+};
+export type ChangeThemeActionType = {
+    type: "SET_THEME_ID";
+    id: string;
+};
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: string): ChangeThemeActionType => ({
+    type: "SET_THEME_ID",
+    id,
+});
